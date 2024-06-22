dde2eba4804cd0e4d88ecabc8b6499d6d27a5931
Baloghlan <qasimovbaloglan13@gmail.com>
2024-06-22 16:02:41 +0400

    card entity CRUD operations

--- src/main/java/com/example/schoolmanagement/controller/CardController.java
+++ src/main/java/com/example/schoolmanagement/controller/CardController.java
@@ -2,10 +2,10 @@ package com.example.schoolmanagement.controller;
 
 import com.example.schoolmanagement.model.CardDto;
 import com.example.schoolmanagement.service.CardService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,32 @@ public class CardController {
 
     private final CardService cardService;
 
+    @Operation(description = "It's for getting all cards")
     @GetMapping
     public List<CardDto> getCards() {
         return cardService.getCards();
+    }
+
+    @Operation(description = "It's for getting card by id")
+    @GetMapping("/{cardId}")
+    public CardDto getCardById(@PathVariable Long cardId){
+        return cardService.getCardById(cardId);
+    }
+
+    @Operation(description = "It's for saving new card")
+    @PostMapping
+    public void saveCard(@RequestBody CardDto cardDto){
+        cardService.saveCard(cardDto);
+    }
+
+    @Operation(description = "It's for editing card by id")
+    @PutMapping("/{cardId}")
+    public void editCard(@PathVariable Long cardId,@RequestBody CardDto cardDto){
+        cardService.editCard(cardId,cardDto);
+    }
+
+    @DeleteMapping("/{cardId}")
+    public CardDto deleteCard(@PathVariable Long cardId){
+        return cardService.deleteCard(cardId);
     }
 }
--- src/main/java/com/example/schoolmanagement/service/CardService.java
+++ src/main/java/com/example/schoolmanagement/service/CardService.java
@@ -2,6 +2,7 @@ package com.example.schoolmanagement.service;
 
 import com.example.schoolmanagement.dao.entity.CardEntity;
 import com.example.schoolmanagement.dao.repository.CardRepository;
+import com.example.schoolmanagement.exceptions.NotFound;
 import com.example.schoolmanagement.maper.CardMapper;
 import com.example.schoolmanagement.model.CardDto;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +27,56 @@ public class CardService {
         log.info("ActionLog.getCards.end");
 
         return cardDtoList;
+    }
+
+    public CardDto getCardById(Long cardId) {
+        log.info("ActionLog.getCardById.start card {}", cardId);
+
+        CardEntity cardEntity = findCard(cardId);
+        CardDto cardDto = cardMapper.mapToDto(cardEntity);
+
+        log.info("ActionLog.getCardById.end card {}", cardId);
+        return cardDto;
+    }
+
+    public void saveCard(CardDto cardDto){
+        log.info("ActionLog.saveCard.start");
+
+        CardEntity cardEntity = cardMapper.mapToEntity(cardDto);
+        cardRepository.save(cardEntity);
+
+        log.info("ActionLog.saveCard.end");
+    }
+
+    public void editCard(Long cardId,CardDto cardDto){
+        log.info("ActionLog.editCard.start card {}", cardId);
+
+        CardEntity cardEntity = findCard(cardId);
+
+        if(cardDto.getCardNumber()!=null){
+            cardEntity.setCardNumber(cardDto.getCardNumber());
+        }
+        if(cardDto.getExpireDate()!=null){
+            cardEntity.setExpireDate(cardDto.getExpireDate());
+        }
+
+        cardRepository.save(cardEntity);
+        log.info("ActionLog.editCard.end card {}", cardId);
+    }
+
+    public CardDto deleteCard(Long cardId){
+        log.info("ActionLog.deleteCard.start card {}",cardId);
+
+        CardEntity cardEntity = findCard(cardId);
+        CardDto cardDto = cardMapper.mapToDto(cardEntity);
+        cardRepository.deleteById(cardId);
+
+        log.info("ActionLog.deleteCard.end card {}",cardId);
+        return cardDto;
+    }
+    private CardEntity findCard(Long cardId) {
+        CardEntity cardEntity = cardRepository.findById(cardId).
+                orElseThrow(() -> new NotFound("CARD_NOT_FOUND", "Error ActionLog.findCard cardId {" + cardId + "}"));
+        return cardEntity;
     }
 }
