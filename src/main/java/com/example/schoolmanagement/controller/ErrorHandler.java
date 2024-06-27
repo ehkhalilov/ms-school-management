0c7c247cdeee5fbc2f9987e870c39b8f2a11b0ff
Baloghlan <qasimovbaloglan13@gmail.com>
2024-06-27 19:00:56 +0400

    liquibase and validation

--- src/main/java/com/example/schoolmanagement/controller/ErrorHandler.java
+++ src/main/java/com/example/schoolmanagement/controller/ErrorHandler.java
@@ -4,9 +4,14 @@ import com.example.schoolmanagement.exceptions.NotFound;
 import com.example.schoolmanagement.model.ExceptionDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -18,4 +23,14 @@ public class ErrorHandler {
         log.error(e.getErrorMessage());
         return new ExceptionDto(e.getCode());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
